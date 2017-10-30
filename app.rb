191d3c483a9f82e29e950cd8f933ec50a72067f9
Flühmann Tobias, INI-ON-NTO-COY-NNI <tobias.fluehmann@swisscom.com>
2017-10-30 19:40:15 +0100

    add favicon

--- app.rb
+++ app.rb
@@ -5,6 +5,15 @@ require 'htmlentities'
 
 set :bind, '0.0.0.0'
 
+def random_cow
+  cows = Cow.cows
+  cows[Random.new.rand(0...cows.count)]
+end
+
 get '/' do
-  "<pre>#{HTMLEntities.new.encode(Cow.new.say(ChuckNorris.random))}</pre>"
+  "<pre>#{HTMLEntities.new.encode(Cow.new(cow: random_cow).say(ChuckNorris.random))}</pre>"
+end
+
+get '/favicon.ico' do
+  File.read('./favicon.ico')
 end
